e8e40578b9ebfb09147d6da1a3ff50370a154e4c
Sahil Saha <35343652+sahilsaha7773@users.noreply.github.com>
2019-07-07 00:26:42 +0530

    heroku

--- unireporter/settings.py
+++ unireporter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,4 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+django_heroku.settings(locals())
